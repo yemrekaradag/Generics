9e629a6f394de96a45294a8b92acb36b44214489
yemrekaradag <yemrekaradag@hotmail.com>
2022-10-30 00:12:43 +0300

    Added Generic Node Class

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            Node<string> node1 = new Node<string>("Mike", null);
+            Node<string> node2 = new Node<string>("Raymond", node1);
+            Console.WriteLine($"Node1 Data: {node1.GetData()}\nNode1 Link: {node1.GetLink()}");
+            Console.WriteLine($"Node2 Data: {node2.GetData()}\nNode2 Link: node1 data: {node2.GetLink().Data} or node1 link: {node2.GetLink().Link}");
+
             int num1 = 100;
             int num2 = 200;
             Console.WriteLine($"num1: {num1}\nnum2: {num2}");
@@ -25,6 +30,36 @@ namespace Generics
             temp = val1;
             val1 = val2;
             val2 = temp;
+        }
+    }
+    public class Node<T>
+    {
+        T data;
+        Node<T> link;
+        public Node(T data, Node<T> link)
+        {
+            this.data = data;
+            this.link = link;
+        }
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+            //set { data = Data; }
+        }
+        public Node<T> Link
+        {
+            get { return link; }
+            set { link = value; }
+            //set { link = Link; }
+        }
+        public T GetData()
+        {
+            return data;
+        }
+        public Node<T> GetLink()
+        {
+            return link;
         }
     }
 }
