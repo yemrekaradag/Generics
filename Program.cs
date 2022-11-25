070c4331e5ce2eebe1768806c0942d816036cb91
Yunus Emre Karadağ <yemrekaradag@hotmail.com>
2022-11-25 23:59:46 +0300

    Add project files.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+
             Node<string> node1 = new Node<string>("Yunus", null);
             Node<string> node2 = new Node<string>("Emre", node1);
             Console.WriteLine($"Node1 Data: {node1.Data}\nNode1 Link: {node1.Link}");
